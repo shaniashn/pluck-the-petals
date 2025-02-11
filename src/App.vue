61b98a814f6f0b91f2dc7461dc3bd023a3a42d90
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-02-11 23:59:07 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+ <MainPage />
+</template>
+
+<script>
+import MainPage from './MainPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  list-style: none;
+}
+</style>
---
+++ src/MainPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="all-flower">
+    <div class="flower">
+    <div class="core-petal"><img src="./assets/core-petal.svg" alt="core-petal"></div>
+  </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
+
+<style>
+.all-flower {
+  background-color: aqua;
+  height: 100vh;
+}
+
+.flower {
+  margin: auto 0;
+}
+.core-petal {
+  width: 10%;
+
+}
+</style>
