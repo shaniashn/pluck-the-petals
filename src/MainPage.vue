175d4841eecd60ec4f1977e0d59b1583e45ef200
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-02-12 23:53:21 +0700

    feat: rotating flower

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,7 +1,8 @@
 <template>
   <section class="all-flower">
     <div class="flower">
-    <div class="core-petal"><img src="./assets/core-petal.svg" alt="core-petal"></div>
+    <!-- <div class="core-petal"><img src="./assets/core-petal.svg" alt="core-petal"></div> -->
+     <div class="flower-ex"><img src="./assets/image.png" alt=""></div>
   </div>
   </section>
 </template>
@@ -14,15 +15,26 @@ export default {
 
 <style>
 .all-flower {
-  background-color: aqua;
   height: 100vh;
+  display: grid;
+  align-items: center;
 }
 
 .flower {
   margin: auto 0;
+  /* background-color: aqua; */
 }
 .core-petal {
   width: 10%;
-
 }
+
+.flower-ex img {
+  transition: transform 2s ease-in-out;
+}
+
+.flower-ex img:hover {
+  transform: rotate(360deg);
+}
+
+
 </style>
