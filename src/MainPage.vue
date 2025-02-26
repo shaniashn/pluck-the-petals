34d1e325d264ac8ce014875b292dabb8f5fb3ba2
Sha Nia Siahaan <shaniashn76@gmail.com>
2025-02-26 21:15:00 +0700

    feat: migrate style to scss

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="all-flower">
-  <h2>pluck the petals</h2>
+    <h2>pluck the petals</h2>
     <div class="flower">
-     <div class="flower-addition"><img src="./assets/flower-addition-.svg" alt=""></div>
-     <div class="flower-core"><img src="./assets/flower-core.svg" alt=""></div>
-  </div>
+      <div class="flower-addition"><img src="./assets/flower-addition-.svg" alt=""></div>
+      <div class="flower-core"><img src="./assets/flower-core.svg" alt=""></div>
+    </div>
   </section>
 </template>
 
@@ -14,50 +14,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .all-flower {
   overflow: hidden;
   height: 100vh;
-}
 
-.flower {
-  /* background-color: rgb(0, 255, 255, 0.5); */
-  width: 100%;
-  height: 100%;
-  /* position: relative; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  .flower {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-.flower .flower-core {
-  /* margin: auto 0; */
-  width: 30%;
-  /* background-color: rgb(0, 55, 255, 0.5); */
-  /* position: relative; */
-  
-  position: absolute;
-}
+    .flower-core {
+      width: 30%;
+      position: absolute;
 
-.flower .flower-addition {
-  width: 50%;
-  transform: translateX(50%);
-  /* justify-content: end; */
-  position: absolute;
-  bottom: 0;
-  /* transition: transform 2s ease-in; */
-}
+      img {
+        animation: rotateFlower 2s 1;
 
-.flower-core img {
-  transition: transform 2s ease-in;
-  /* background-color: black; */
-  transform: rotate(360deg), rotate(90deg);
-}
+        &:hover {
+          transform: rotate(360deg);
+          animation: rotateFlower 2s infinite;
+        }
+      }
+    }
 
-.flower-core img:hover {
-  /* transform: rotate(360deg); */
-  /* background-color: aqua; */
-  /* animation: rotateFlower 2s infinite; */
+    .flower-addition {
+      width: 50%;
+      transform: translateX(50%);
+      position: absolute;
+      bottom: 0;
+    }
+  }
 }
 
 @keyframes rotateFlower {
